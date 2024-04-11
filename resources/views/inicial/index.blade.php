02053e030d5665757cc198b58a3685ab1efa2821
Leandro <leandrodev83@gmail.com>
2024-04-11 07:56:50 -0300

    Instala bootstrap

---
+++ resources/views/inicial/index.blade.php
@@ -0,0 +1,24 @@
+@extends('system.app')
+
+@section('title', 'Painel Inicial')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <p class="alert alert-info">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, voluptates.
+                @guest    
+                    <a class="btn btn-success btn-sm" href="{{ route('login') }}">Login</a>
+                    <a class="btn btn-primary btn-sm" href="{{ route('register') }}">Cadastre-se</a>
+                @endguest
+
+                @auth
+                    <a class="btn btn-primary btn-sm" href="{{ route('dashboard') }}">Painel Inicial</a>
+                @endauth
+            </p>
+        </div>
+    </div>
+</div>
+@endsection
+
---
+++ resources/views/system/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title') - Sistema de gestão de livros</title>
+
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
