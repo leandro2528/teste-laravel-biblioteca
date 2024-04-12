436f343c3a6bd39d827f7fb89301d5d668d376be
Leandro <leandrodev83@gmail.com>
2024-04-12 07:44:33 -0300

    atualiza chamada para a pagina inicial

---
+++ resources/views/livros/reserva.blade.php
@@ -0,0 +1,37 @@
+<x-app-layout>
+    
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="row">
+                        ols
+                    </div>
+                   
+                    <div class="row my-5">
+                        <div class="col">
+                            <div class="card">
+                                <div class="card-header">
+                                    <i class="fas fa-table me-1"></i>
+                                        <span>Livros reservados</span>
+                                        <span>
+                                            <a class="btn btn-primary btn-sm ms-4" href="{{ route('livros-create') }}">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                                                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+                                                </svg>
+                                            </a>
+                                        </span>
+                                </div>
+                                <div class="card-body">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</x-app-layout>
